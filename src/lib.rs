8b9cf2604801645615a8219b637f8d10c660468b
Markus Becker <mtib.becker@gmail.com>
2021-03-12 21:38:21 +0100

    Dirty u64 hashmaps

--- src/lib.rs
+++ src/lib.rs
@@ -81,20 +81,74 @@ pub mod mdp;
 pub mod strategy;
 
 use mdp::{Agent, State};
-use std::collections::HashMap;
+use std::{
+    collections::{hash_map::DefaultHasher, HashMap},
+    hash::Hash,
+    hash::Hasher,
+    marker::PhantomData,
+};
 use strategy::explore::ExplorationStrategy;
 use strategy::learn::LearningStrategy;
 use strategy::terminate::TerminationStrategy;
+#[derive(PartialEq, Eq, Hash, Copy, Clone)]
+pub struct StateHash(u64);
+
+impl<S> From<&S> for StateHash
+where
+    S: State,
+{
+    fn from(state: &S) -> Self {
+        let mut hasher = DefaultHasher::new();
+        state.hash(&mut hasher);
+        StateHash(hasher.finish())
+    }
+}
+
+impl From<u64> for StateHash {
+    fn from(v: u64) -> Self {
+        StateHash(v)
+    }
+}
+
+impl From<StateHash> for u64 {
+    fn from(sh: StateHash) -> Self {
+        sh.0
+    }
+}
+
+#[derive(PartialEq, Eq, Hash, Copy, Clone)]
+pub struct ActionHash(u64);
+
+impl From<u64> for ActionHash {
+    fn from(v: u64) -> Self {
+        ActionHash(v)
+    }
+}
+
+impl From<ActionHash> for u64 {
+    fn from(ah: ActionHash) -> Self {
+        ah.0
+    }
+}
+
+impl<A> From<&A> for ActionHash
+where
+    A: Eq + Hash + Clone,
+{
+    fn from(action: &A) -> Self {
+        let mut hasher = DefaultHasher::new();
+        action.hash(&mut hasher);
+        ActionHash(hasher.finish())
+    }
+}
 
 /// An `AgentTrainer` can be trained for using a certain [Agent](mdp/trait.Agent.html). After
 /// training, the `AgentTrainer` contains learned knowledge about the process, and can be queried
 /// for this. For example, you can ask the `AgentTrainer` the expected values of all possible
 /// actions in a given state.
-pub struct AgentTrainer<S>
-where
-    S: State,
-{
-    q: HashMap<S, HashMap<S::A, f64>>,
+pub struct AgentTrainer<S> {
+    q: HashMap<u64, HashMap<u64, f64>>,
+    state_marker: PhantomData<S>,
 }
 
 impl<S> AgentTrainer<S>
@@ -102,25 +156,30 @@ where
     S: State,
 {
     pub fn new() -> AgentTrainer<S> {
-        AgentTrainer { q: HashMap::new() }
+        AgentTrainer {
+            q: HashMap::new(),
+            state_marker: PhantomData,
+        }
     }
     /// Fetches the learned values for the given state, by `Action`, or `None` if no value was
     /// learned.
-    pub fn expected_values(&self, state: &S) -> Option<&HashMap<S::A, f64>> {
+    pub fn expected_values(&self, state: &S) -> Option<&HashMap<u64, f64>> {
         // XXX: make associated const with empty map and remove Option?
-        self.q.get(state)
+        self.q.get(&StateHash::from(state).0)
     }
     /// Fetches the learned value for the given `Action` in the given `State`, or `None` if no
     /// value was learned.
     pub fn expected_value(&self, state: &S, action: &S::A) -> Option<f64> {
-        self.q.get(state).and_then(|m| m.get(action).copied())
+        self.q
+            .get(&StateHash::from(state).0)
+            .and_then(|m| m.get(&ActionHash::from(action).0).copied())
     }
     /// Returns a clone of the entire learned state to be saved or used elsewhere.
-    pub fn export_state(&self) -> HashMap<S, HashMap<S::A, f64>> {
+    pub fn export_state(&self) -> HashMap<u64, HashMap<u64, f64>> {
         self.q.clone()
     }
     /// Imports a state, completely replacing any learned progress
-    pub fn import_state(&mut self, q: HashMap<S, HashMap<S::A, f64>>) {
+    pub fn import_state(&mut self, q: HashMap<u64, HashMap<u64, f64>>) {
         self.q = q;
     }
     /// Trains this [AgentTrainer] using the given [ExplorationStrategy], [LearningStrategy] and
@@ -141,14 +200,21 @@ where
             let r_t_next = s_t_next.reward();
 
             let v = {
-                let old_value = self.q.get(&s_t).and_then(|m| m.get(&action));
-                learning_strategy.value(&self.q.get(s_t_next), &old_value, r_t_next)
+                let old_value = self
+                    .q
+                    .get(&StateHash::from(&s_t).0)
+                    .and_then(|m| m.get(&ActionHash::from(&action).0));
+                learning_strategy.value(
+                    &self.q.get(&StateHash::from(s_t_next).0),
+                    &old_value,
+                    r_t_next,
+                )
             };
 
             self.q
-                .entry(s_t)
+                .entry(StateHash::from(&s_t).0)
                 .or_insert_with(HashMap::new)
-                .insert(action, v);
+                .insert(ActionHash::from(&action).0, v);
 
             if termination_strategy.should_stop(&s_t_next) {
                 break;
